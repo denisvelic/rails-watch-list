47f0797398a09a4f749f4535b2cf5a2d7c97d94a
Denis Velic <denis.velic@laposte.net>
2022-11-17 17:01:05 +0100

    added models, controllers

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,34 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[create]
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.delete
+    redirect_to bookmark_path, status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
